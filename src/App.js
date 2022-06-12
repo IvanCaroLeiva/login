42e36e2c1f8228d081786de9efa9804ff1fdb0d2
Reimon <ramon.fuentes.alarcon@gmail.com>
2022-06-12 02:02:35 +0100

    input the username y contrasena fueron agregados.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 function App() {
   return (
-   <p>espero aprender </p>
+   <div style={{
+      marginTop: 10,
+      marginLeft: 10,
+      width: 500,
+      display: 'flex',
+      flexDirection: 'column'
+   }}>
+    <div>
+      <span>Ingrese Usuario qlo:</span>
+      <input
+        type='text'
+      />
+    </div>
+    <div style={{ marginTop: 5 }}>
+      <span>Ingrese Contrasenia el qlo:</span>
+      <input
+        type='password'
+      />
+    </div>
+   </div>
   );
 }
 
